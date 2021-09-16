30b31166da2e21794d5e91415a5ebbf90209bd8c
Chen Zhung <46520696+Xectroz@users.noreply.github.com>
2021-09-16 22:07:09 +0800

    changed db

--- square.sql
+++ square.sql
@@ -1,107 +1,233 @@
-CREATE TABLE `Users` (
-	`user_id` INT(11) NOT NULL AUTO_INCREMENT,
-	`username` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`isAdmin` BOOLEAN(0) NOT NULL DEFAULT '0',
-	`field` varchar(255) NOT NULL,
-	`organization` varchar(255) NOT NULL,
-	PRIMARY KEY (`user_id`)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2021 at 04:06 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
-CREATE TABLE `Wallet` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
-	`balance` FLOAT(11,2) NOT NULL,
-	`expenses` FLOAT(11,2) NOT NULL,
-	`food` FLOAT(11,2) NOT NULL,
-	`entertainment` FLOAT(11,2) NOT NULL,
-	`accessories` FLOAT(11,2) NOT NULL,
-	`others` FLOAT(11,2) NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Task` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
-	`organization_id` INT(11) NOT NULL,
-	`title` varchar(255) NOT NULL,
-	`description` varchar(255) NOT NULL,
-	`image` varchar(255) NOT NULL,
-	`due_date` DATE NOT NULL,
-	`done` BOOLEAN NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Event` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
-	`organization_id` INT(11) NOT NULL,
-	`title` varchar(255) NOT NULL,
-	`description` varchar(255) NOT NULL,
-	`date` DATE NOT NULL,
-	`image` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Organization` (
-	`id` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`member_count` INT(11) NOT NULL,
-	`member_balance` FLOAT(11,2) NOT NULL,
-	`member_expenses` FLOAT(11,2) NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Goals` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
-	`organization_id` INT(11) NOT NULL,
-	`title` varchar(255) NOT NULL,
-	`description` varchar(255) NOT NULL,
-	`achieved` BOOLEAN NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Transactions` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`wallet_id` FLOAT(11,2) NOT NULL,
-	`amount` FLOAT(11,2) NOT NULL,
-	`meal_id` INT(11) NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Meal` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`price` FLOAT(11,2) NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-ALTER TABLE `Users` ADD CONSTRAINT `Users_fk0` FOREIGN KEY (`organization`) REFERENCES `Organization`(`id`);
-
-ALTER TABLE `Wallet` ADD CONSTRAINT `Wallet_fk0` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`);
-
-ALTER TABLE `Task` ADD CONSTRAINT `Task_fk0` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`);
-
-ALTER TABLE `Task` ADD CONSTRAINT `Task_fk1` FOREIGN KEY (`organization_id`) REFERENCES `Organization`(`id`);
-
-ALTER TABLE `Event` ADD CONSTRAINT `Event_fk0` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`);
-
-ALTER TABLE `Event` ADD CONSTRAINT `Event_fk1` FOREIGN KEY (`organization_id`) REFERENCES `Organization`(`id`);
-
-ALTER TABLE `Goals` ADD CONSTRAINT `Goals_fk0` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`);
-
-ALTER TABLE `Goals` ADD CONSTRAINT `Goals_fk1` FOREIGN KEY (`organization_id`) REFERENCES `Organization`(`id`);
-
-ALTER TABLE `Transactions` ADD CONSTRAINT `Transactions_fk0` FOREIGN KEY (`wallet_id`) REFERENCES `Wallet`(`id`);
-
-ALTER TABLE `Transactions` ADD CONSTRAINT `Transactions_fk1` FOREIGN KEY (`meal_id`) REFERENCES `Meal`(`id`);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `square`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `event`
+--
 
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `organization_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `goals`
+--
 
+CREATE TABLE `goals` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `organization_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `achieved` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization`
+--
+
+CREATE TABLE `organization` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `member_count` int(11) NOT NULL,
+  `member_balance` float(11,2) NOT NULL,
+  `member_expenses` float(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `organization_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `dueDate` date NOT NULL,
+  `done` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  `field` varchar(255) NOT NULL,
+  `organization` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `balance` float(11,2) NOT NULL,
+  `food` float(11,2) NOT NULL,
+  `accessories` float(11,2) NOT NULL,
+  `entertainment` float(11,2) NOT NULL,
+  `education` float(11,2) NOT NULL,
+  `others` float(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `organization_id` (`organization_id`);
+
+--
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `organization_id` (`organization_id`);
+
+--
+-- Indexes for table `organization`
+--
+ALTER TABLE `organization`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `organization_id` (`organization_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `organization` (`organization`);
+
+--
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `goals`
+--
+ALTER TABLE `goals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wallet`
+--
+ALTER TABLE `wallet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `event_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`);
+
+--
+-- Constraints for table `goals`
+--
+ALTER TABLE `goals`
+  ADD CONSTRAINT `goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `goals_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`);
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `task_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`organization`) REFERENCES `organization` (`id`);
+
+--
+-- Constraints for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
